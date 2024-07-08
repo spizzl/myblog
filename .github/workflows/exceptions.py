2d99941fd19d65dcd1e7dae5330db28dfcd9ccda
Florentini Scampini <florin@Florentinis-MacBook-Pro.local>
2024-07-08 15:18:02 +0200

    Added Workflow

---
+++ .github/workflows/exceptions.py
@@ -0,0 +1,39 @@
+class NoElementFound(Exception):
+    """Kein Element Gefunden :(("""
+    pass
+class VerificationNeeded(Exception):
+    """Further Verification Needed :(("""
+    pass
+class AccountVerificationNeeded(Exception):
+    """Further Verification Needed :(("""
+    pass
+class CheaterDetected(Exception):
+    """They got us... :(("""
+    pass
+class CaptchaNeeded(Exception):
+    """Captcha muss gemacht werden"""
+    pass
+class AudioPuzzleBlocked(Exception):
+    """Kein Audiocaptcha"""
+    pass
+class ButtonNotClickable(Exception):
+    """Kann nicht gedrückt werden :(("""
+    pass
+class CaptchaUnsolved(Exception):
+    """Kann nicht gedrückt werden :(("""
+    pass
+class HashError(Exception):
+    """Kann nicht gedrückt werden :(("""
+    pass
+class NoEntryError(Exception):
+    """Kein  """
+    pass
+class CaptchaError(Exception):
+    """ IRGENDWAS FALSCH """
+    pass
+class Finish(Exception):
+    """ IRGENDWAS FALSCH """
+    pass
+class ServerDownError(Exception):
+    """ IRGENDWAS FALSCH """
+    pass
