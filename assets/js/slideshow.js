348ac3b8c2818fec8ea8c6a864c92c28455f371d
spizzl <florin@woeginger.de>
2025-11-19 03:57:52 +0100

    please work

---
+++ assets/js/slideshow.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let currentFace = 0;
+  let faces = 4;
+  cube = document.getElementById('cube');
+
+
+  function showFace(faceIndex) {
+    currentFace = faceIndex;
+    updateCube();
+  }
+
+  function nextFace() {
+    console.log('Next face');
+    currentFace = (currentFace + 1) % faces;
+    updateCube();
+  }
+
+
+  function updateCube() {
+    const rotationY = -currentFace * 90;
+    cube.style.transform = `rotateY(${rotationY}deg)`;
+  }
+
+
+});
+
