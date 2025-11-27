f30af09c6adab16ba0e369613f9089fadac7f4fb
spizzl <florin@woeginger.de>
2025-11-27 05:12:43 +0100

    Background update

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,38 +1,53 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   let currentFace = 0;
-  const cube = document.getElementById('cube');
-  const tiltWrapper = document.getElementById('tilt-wrapper');
-  
+  const cube = document.getElementById("cube");
+  const tiltWrapper = document.getElementById("tilt-wrapper");
 
+  function triggerSkills() {
+    const normalizedIndex = ((currentFace % 4) + 4) % 4;
+
+    const bars = document.querySelectorAll(".progress-fill");
+    if (normalizedIndex === 1) {
+      bars.forEach((bar) => {
+        const w = bar.getAttribute("data-width");
+        bar.style.width = w;
+      });
+    } else {
+      bars.forEach((bar) => {
+        bar.style.width = "0";
+      });
+    }
+  }
   function updateCube() {
     const rotationY = -currentFace * 90;
     cube.style.transform = `rotateY(${rotationY}deg)`;
+    triggerSkills();
   }
 
-  document.getElementById('arrow-left').addEventListener('click', () => {
+  document.getElementById("arrow-left").addEventListener("click", () => {
     currentFace--;
     updateCube();
   });
 
-  document.getElementById('arrow-right').addEventListener('click', () => {
+  document.getElementById("arrow-right").addEventListener("click", () => {
     currentFace++;
     updateCube();
   });
 
-  document.addEventListener('mousemove', (e) => {
+  document.addEventListener("mousemove", (e) => {
     const x = e.clientX / window.innerWidth;
     const y = e.clientY / window.innerHeight;
-    
-    const tiltX = (y - 0.5) * 20; 
-    const tiltY = (x - 0.5) * 20; 
+
+    const tiltX = (y - 0.5) * 20;
+    const tiltY = (x - 0.5) * 20;
 
     tiltWrapper.style.transform = `rotateX(${-tiltX}deg) rotateY(${tiltY}deg)`;
   });
-  
-  if(window.DeviceOrientationEvent) {
-    window.addEventListener('deviceorientation', (event) => {
+
+  if (window.DeviceOrientationEvent) {
+    window.addEventListener("deviceorientation", (event) => {
       if (!event.beta) return;
-      const tiltX = Math.min(Math.max(event.beta - 45, -20), 20); 
+      const tiltX = Math.min(Math.max(event.beta - 45, -20), 20);
       const tiltY = Math.min(Math.max(event.gamma, -20), 20);
       tiltWrapper.style.transform = `rotateX(${-tiltX}deg) rotateY(${tiltY}deg)`;
     });
